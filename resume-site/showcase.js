491e840ef0e5f467a8d576740427e0613406cb16
RyanMcKelvey <ryanmckelvey98@gmail.com>
2021-06-08 16:30:25 +0100

    added basic functionality with github

--- resume-site/showcase.js
+++ resume-site/showcase.js
@@ -11,7 +11,21 @@ function getRepos() {
     }
 }
 getRepos().then((result) => {
+    var index = 0
     result.forEach(repo => {
-        
+        if (index <= 6) {
+            var div = document.createElement('div');
+            div.setAttribute('id', 'project');
+            div.innerHTML = `
+            <a>
+            <h5>made with loop :o ${index}</h5>
+            <p>description description description</p>
+            <p>Created date</p>
+            <a>Linky boy</a>
+            </a>
+            `;
+            document.getElementById('projects').appendChild(div)
+            index++;
+        }
     });
 });
