baf4a07e071795231b6c4cd5a53fe0079a99d8a4
Ryan McKelvey <ryanmckelvey98@gmail.com>
2022-01-26 11:58:47 +0000

    implementing github scrape script

---
+++ resume-site-v2/main.js
@@ -0,0 +1,34 @@
+function getRepos() {
+    try {
+        var repos = fetch("https://api.github.com/users/ryanmckelvey/repos");
+        return repos.then(async(list) => {
+            var repoList = list.json();
+            const x = await repoList;
+            return x.reverse();
+        })
+    } catch {
+        console.log("error retrieving repos")
+    }
+}
+getRepos().then((result) => {
+    var index = 0
+    result.forEach(repo => {
+        if (index <= 4) {
+            console.log(repo)
+            var url = "https://github.com/ryanmckelvey/" + repo.name
+            var div = document.createElement('div');
+            div.setAttribute('class', 'project');
+            div.innerHTML = `
+            <a href="${url}" style="text-decoration: none; color: var(--jet);">
+            <h3 class="project_name">${repo.name}</h3>
+            <p>${repo.language}
+            <p>${repo.description}</p>
+            <p>${repo.created_at}</p>
+            <a href="${url}"><i class="fab fa-github"></i></a>
+            </a>
+            `;
+            document.getElementById('projects').appendChild(div)
+            index++;
+        }
+    });
+});
