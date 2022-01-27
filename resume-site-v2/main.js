e7892b3b7efc14a4da58442714d84d3df0844474
Ryan McKelvey <ryanmckelvey98@gmail.com>
2022-01-27 15:34:54 +0000

    updating responsiveness, adding contact option, reformatting github scraper

--- resume-site-v2/main.js
+++ resume-site-v2/main.js
@@ -14,17 +14,17 @@ getRepos().then((result) => {
     var index = 0
     result.forEach(repo => {
         if (index <= 4) {
-            console.log(repo)
+            //console.log(repo)
             var url = "https://github.com/ryanmckelvey/" + repo.name
             var div = document.createElement('div');
+            var date = repo.created_at.split('T')
             div.setAttribute('class', 'project');
             div.innerHTML = `
             <a href="${url}" style="text-decoration: none; color: var(--jet);">
             <h3 class="project_name">${repo.name}</h3>
             <p>${repo.language}
             <p>${repo.description}</p>
-            <p>${repo.created_at}</p>
-            <a href="${url}"><i class="fab fa-github"></i></a>
+            <p>${date[0]}</p>
             </a>
             `;
             document.getElementById('projects').appendChild(div)
